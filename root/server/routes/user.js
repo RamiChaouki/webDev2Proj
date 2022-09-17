d9f384fb1ae369848b95f5169ef9e6786f06defd
SpicyRiceTea <chaoukirami@live.ca>
2022-09-17 14:37:44 -0400

    working backend for registration, no server side validation yet

--- root/server/routes/user.js
+++ root/server/routes/user.js
@@ -1,7 +1,37 @@
 const express=require('express');
 const router=express.Router();
+const bcrypt=require('bcrypt');
+const Users=require('../models/user');
 
-router.post('/Register',(req,res)=>{
+//TODO:
+/**
+ * middleware to make sure FIELDS NOT NULL
+ * middleware to make sure PASSWORD IS LARGER THAN 6 CHARACTERS
+ * middleware to make sure USERNAME and EMAIL DON'T ALREADY EXIST
+ */
+
+
+
+router.post('/Register',async (req,res)=>{
+const {firstName, lastName, username, email, password, dateOfBirth,status}=req.body;
+bcrypt.hash(password,10)
+    .then((hash)=>{
+                    Users.create({
+                        firstName:firstName,
+                        lastName:lastName,
+                        username:username,
+                        email:email,
+                        password:hash,
+                        dateOfBirth:dateOfBirth,
+                        status:status
+                    })
+        .catch((error)=>{
+            console.log(error);
+            // res.status(500).json({"Error":"Error: "+ error})
+        });
+res.status(201).json({"Message":"User "+username+" created"});
+
+})
 
 });
 
