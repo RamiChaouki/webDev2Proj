045f7eed1bcdbf98b21fb3f8ea1005c1d9ca391c
SpicyRiceTea <chaoukirami@live.ca>
2022-09-17 18:09:57 -0400

    Fixed Kyle's edge case where user submits only one field during registration(confirmActDoesntExist.js) by adding try/catch block

--- root/server/routes/middleware/confirmActDoesntExist.js
+++ root/server/routes/middleware/confirmActDoesntExist.js
@@ -4,15 +4,19 @@ const ErrorApi = require('./errorHandling/ErrorApi');
 
 async function ConfirmActDoesntExists(req,res,next){
     const {username, email} = req.body;
-    const account=await Users.findOne({
-                                        where:{
-                                            [Op.or]:[
-                                                {username:username},
-                                                {email:email}
-                                            ]
-                                        }
-    });
-    
+    try{
+        const account=await Users.findOne({
+                                            where:{
+                                                [Op.or]:[
+                                                    {username:username},
+                                                    {email:email}
+                                                ]
+                                            }
+        });
+    }catch(error){
+        next(error);
+        return;
+    }
     if(account){
         account.dataValues.username==username
             ?next(ErrorApi.conflict('Username Already Taken'))
