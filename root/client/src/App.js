407fc390f9ed14a841fe9de2df995b8198fcd139
SpicyRiceTea <chaoukirami@live.ca>
2022-09-18 16:22:39 -0400

    finished registration page with frontend validation

--- root/client/src/App.js
+++ root/client/src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+
+//PAGES AND COMPONENTS
+import Registration from './components/registration/Registration';
+import Home from './components/home/Home';
+
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/Home' element={<Home/>}></Route>
+          <Route path='/Register' element={<Registration/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
