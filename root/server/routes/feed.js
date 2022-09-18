b1670d692ff8d178db513c68de6a176a97c2f8c0
Kyle Piché <kylepiche@gmail.com>
2022-09-17 22:57:42 -0400

    Fixed confirmaccount middleware

--- root/server/routes/feed.js
+++ root/server/routes/feed.js
@@ -9,11 +9,19 @@ const Posts=require('../models/post');
 const apiErrorHandler=require('./middleware/errorHandling/apiErrorHandler');
 
 //TODO: GET FEED
-// router.get("/", async (req, res)=>{
+router.get("/", async (req, res)=>{
 
-// });
+});
 
 //TODO: POST FEED/POST
+router.post("/", apiErrorHandler, async (req, res)=>{
+    
+    const post = req.body;
+    post.userId = 1; //For testing purposes
+    await Posts.create(post);
+    res.json(post);
+
+});
 
 //TODO: POST FEED/COMMENT/:postId
 
