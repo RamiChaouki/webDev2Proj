28a3382b0f82643f7b0400fa275177fccf062964
SpicyRiceTea <chaoukirami@live.ca>
2022-09-19 09:58:00 -0400

    Solved bug in seeder, passwords now encrypted

--- root/server/seeders/userSeeder.js
+++ root/server/seeders/userSeeder.js
@@ -1,6 +1,7 @@
 const Users = require('../models/user');
 const {sequelize} = require('../models/sequelizeConfig');
 const axios = require('axios');
+const bcrypt = require('bcrypt');
 
 var results;
 
@@ -8,18 +9,26 @@ axios
     .get('https://randomuser.me/api/?inc=name,email,login,dob&&results=50')
     .then((res)=>{
         results=Object.entries(res.data.results);
+
         for(let i=0; i<50;i++){
-            Users.create({
-                firstName:results[i][1].name.first,
-                lastName:results[i][1].name.last,
-                username:results[i][1].login.username,
-                email:results[i][1].email,
-                password:"testtest",
-                dateOfBirth:results[i][1].dob.date,
-                status:'active'
-            })
-}
-        })
+            bcrypt.hash("testtest",10)
+                .then((hash)=>{
+                    Users.create({
+                        firstName:results[i][1].name.first,
+                        lastName:results[i][1].name.last,
+                        username:results[i][1].login.username,
+                        email:results[i][1].email,
+                        password:hash,
+                        dateOfBirth:results[i][1].dob.date,
+                        status:'active'
+                    })
+                })
+                .catch((error)=>{
+                    console.log(error);
+                    
+                });
+        } 
+    })
     .catch((error)=>{
         console.log("Seeder failed");
         console.log(error);
