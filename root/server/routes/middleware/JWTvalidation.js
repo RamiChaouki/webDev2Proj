ebe5ff7144b127a67fb15946264ad9b944fda703
SpicyRiceTea <chaoukirami@live.ca>
2022-09-19 10:53:56 -0400

    updated JWTvalidation to use an environemnt variable

--- root/server/routes/middleware/JWTvalidation.js
+++ root/server/routes/middleware/JWTvalidation.js
@@ -1,11 +1,12 @@
 const {verify} = require('jsonwebtoken');
+require('dotenv').config();
 
 const validateToken = (req, res, next)=>{
     const accessToken = req.header("accessToken");
 
     if(!accessToken){return res.json({error: "User not logged in!"})};
     try {
-        const validToken = verify(accessToken, "importantsecret")
+        const validToken = verify(accessToken, process.env.JWT_PRIVATE_KEY)
         req.user = validToken;
         if(validToken){
             return next();
