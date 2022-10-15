68617e5eb7728d83dd8af3b77ee363ac10aa183f
CocoScrappy <103375684+CocoScrappy@users.noreply.github.com>
2022-10-14 20:07:20 -0400

    Update admin
    cleaning up, annotating code

--- root/server/routes/admin.js
+++ root/server/routes/admin.js
@@ -9,21 +9,19 @@ const confirmActDoesntExist = require("./middleware/confirmActDoesntExist");
 const confirmNoEmptyField = require("./middleware/confirmNoEmptyField");
 const registrationHandler = require("./middleware/registrationHandler");
 const validateToken = require("./middleware/JWTvalidation");
-//HASHING DEPENDENCY
 const bcrypt=require('bcrypt');
 
-//npm install react-paginate
-//in App.js - import React Paginate from "react-paginate";
 
 
-
-//Throws ERROR if user doesn't exist
+/**
+ * Middleware
+ * Throws ERROR if user doesn't exist
+*/
 async function doesUserExist(req,res,next){
     
     const id = req.params.id;
     const result = await user.findByPk(id);
     if(result===null){
-        // res.status(400).send("No such user found!");
         req.doesUserExist='false';
         next(ErrorApi.badRequest('No such user found'));
         return;
@@ -32,7 +30,9 @@ async function doesUserExist(req,res,next){
     next();
 }
 
-// returns count of total number of users
+/**
+ * returns count of total number of users
+*/
 async function getUserCount(req,res,next){
     const {count, rows} = await user.findAndCountAll();
     req.count=`Users 0-5/${count}`;
@@ -43,32 +43,98 @@ async function getUserCount(req,res,next){
 /**
 *   ROUTES
 */
-// (GET)/Admin/Users - return info for a list of all users
-// router.get('/Users',validateToken, getUserCount,async(req,res)=>{
-router.get('/Users', getUserCount, async(req,res,next)=>{  //testing line - original above. Tested: Validates token - Good, empty table - Good, 
-    const listOfUsers = await user.findAll();
+/**
+ * (GET)/Admin/Users - return info for a list of all users
+*/
+router.get('/Users', getUserCount, async(req,res,next)=>{  
     res.setHeader('Access-Control-Expose-Headers', 'Content-Range');
     res.setHeader('Content-Range', req.count);
     res.json(listOfUsers);
 });
 
 
-// (GET)/Admin/User/id - return info for one user (when clicking on author when browsing posts or when clicking edit)
-// router.get('/User/:id', validateToken, doesUserExist, apiErrorHandler, async(req, res, next)=>{
-    router.get('/Users/:id', doesUserExist, apiErrorHandler, async(req, res, next)=>{  //testing line - original above. Tested: Validates token - Good, empty table - Good,broken param - returns empty arr, proper param - Good
-
+/**
+* (GET)/Admin/User/id - return info for one user (when clicking on author when browsing posts or when clicking edit)
+*/
+router.get('/Users/:id', doesUserExist, apiErrorHandler, async(req, res, next)=>{
     const id = req.params.id;
     if (req.doesUserExist==='true'){
         const userInfo = await user.findByPk(id); // MUST RESERVE FIELDS FOR EDIT!
         res.json(userInfo);
-    } else{
+    } else {
         res.status(ErrorApi.code).send(ErrorApi.msg);
     }
 });
 
-// (GET)/Admin//Users?... - SCRAP code for getting pagination to work
+
+
+    /**
+     * (POST)/Admin/User - create a new user
+     */ 
+    router.post("/Users", confirmNoEmptyField, confirmActDoesntExist, registrationHandler, apiErrorHandler, async (req, res) => {
+        const { firstName, lastName, username, email, password, dateOfBirth, status, role } = req.body;
+        bcrypt.hash(password, 10)
+        .then(async( hash) => {
+            await user.create({
+            firstName: firstName,
+            lastName: lastName,
+            username: username,
+            email: email,
+            password: hash,
+            dateOfBirth: dateOfBirth,
+            status: status,
+            role: role,
+            })
+        })
+        .catch((error) => {
+            res.status(ErrorApi.code).send(ErrorApi.msg);
+        })
+        .then(async ()=>{ 
+            await user.findOne({
+                where: {
+                    username: username
+                }
+            })
+            .then((result)=>{res.json(result)})
+        });
+    });
+
+    /**
+     * (PUT)/Admin/User/:id - updates the user record from the edited line
+     */
+    router.put('/Users/:id', doesUserExist, ensureNoDuplicates, apiErrorHandler, async(req,res)=>{ 
+        const id = req.params.id;
+        const newValues = req.body;
+        if (req.doesUserExist==='true'){
+            user.update({ id: `${newValues.id}`, firstName: `${newValues.firstName}`, lastName: `${newValues.lastName}`, 
+            username: `${newValues.username}`, email: `${newValues.email}`, dateOfBirth: `${newValues.dateOfBirth}`, status: `${newValues.status}`, role: `${newValues.role}`}, {where: {id:`${id}`}});
+            const result = await user.findByPk(id);
+            res.json(result);
+        } else {
+            res.status(ErrorApi.code).send(ErrorApi.msg);
+        }
+    });
+
+
+    /**
+     * (DELETE)/Admin/User/:userId - deletes selected user by pressing Delete
+     */
+    router.delete('/Users/:id',  doesUserExist, apiErrorHandler, async(req,res,next)=>{
+        const id = req.params.id;
+        await user.destroy({where: {id:`${id}`}});
+        if (user === 0) {
+            next(badRequest('No User with such ID exists'));
+        } else res.json(user);
+    });
+
+
+
+
+//------------------------TODO:-------------------------
+
+// (GET)/Admin//Users?... - Handle pagination, sorting and filtering of the list of users, using query parameters
 // router.get('/User/:id', validateToken, doesUserExist, apiErrorHandler, async(req, res, next)=>{
-// //    router.get('/Users?sort=[field, order]&range=[page, perPage]&filter={"title":"bar"}', doesUserExist, apiErrorHandler, async(req, res, next)=>{  //testing line - original above. Tested: Validates token - Good, empty table - Good,broken param - returns empty arr, proper param - Good       
+// //    router.get('/Users?sort=[field, order]&range=[page, perPage]&filter={"title":"bar"}', doesUserExist, apiErrorHandler, async(req, res, next)=>{  
 
 //     var _a = params.pagination,
 //     page = _a.page,
@@ -102,65 +168,11 @@ router.get('/Users', getUserCount, async(req,res,next)=>{  //testing line - orig
 //     };
 // });
 
+//------------------------UNUSED ROUTES DUE TO USING REACT-ADMIN:-------------------------
+/**
+ * (GET)/Admin/Admins/:role - return info for a filtered list by role
+ */
 
-
-        router.post("/Users", confirmNoEmptyField, confirmActDoesntExist, registrationHandler, apiErrorHandler, async (req, res) => {
-          const { firstName, lastName, username, email, password, dateOfBirth, status, role } = req.body;
-
-          bcrypt.hash(password, 10)
-          .then(async( hash) => {
-                await user.create({
-                firstName: firstName,
-                lastName: lastName,
-                username: username,
-                email: email,
-                password: hash,
-                dateOfBirth: dateOfBirth,
-                status: status,
-                role: role,
-                })
-            })
-            .catch((error) => {
-                res.status(ErrorApi.code).send(ErrorApi.msg);
-            })
-            .then(async ()=>{ 
-                await user.findOne({
-                    where: {
-                      username: username
-                    }
-                })
-                .then((result)=>{res.json(result)})
-            });});
-
-
-// (PUT)/Admin/User/:id - updates the user record from the edited line
-// router.patch('/User/:id', validateToken, doesUserExist, EnsureNoDuplicates, apiErrorHandler, async(req,res)=>{
-    router.put('/Users/:id', doesUserExist, ensureNoDuplicates, apiErrorHandler, async(req,res)=>{ 
-        const id = req.params.id;
-        const newValues = req.body;
-        if (req.doesUserExist==='true'){
-            user.update({ id: `${newValues.id}`, firstName: `${newValues.firstName}`, lastName: `${newValues.lastName}`, 
-            username: `${newValues.username}`, email: `${newValues.email}`, dateOfBirth: `${newValues.dateOfBirth}`, status: `${newValues.status}`, role: `${newValues.role}`}, {where: {id:`${id}`}});
-            const result = await user.findByPk(id);
-            res.json(result);
-        } else {
-            res.status(ErrorApi.code).send(ErrorApi.msg);
-        }
-    });
-
-
-// (DELETE)/Admin/User/:userId - deletes selected user by pressing Delete
-router.delete('/Users/:id',  doesUserExist, apiErrorHandler, async(req,res,next)=>{
-
-    const id = req.params.id;
-    await user.destroy({where: {id:`${id}`}});
-    if (user === 0) {
-      next(badRequest('No User with such ID exists'));
-    } else res.json(user);
-});
-
-//------------------------Unused routes-------------------------
-// (GET)/Admin/Admins/:role - return info for a filtered list by role
 router.get('/filterByRole/:role', validateToken, async(req,res)=>{
    
     const role = req.params.role;
@@ -170,10 +182,10 @@ router.get('/filterByRole/:role', validateToken, async(req,res)=>{
     res.json(filteredList);
 });
 
-// (GET)/Admin/Admins/:role - return info for a filtered list by status
+/**
+ * (GET)/Admin/Admins/:role - return info for a filtered list by status
+ */
 router.get('/filterByStatus/:status', validateToken, async(req,res)=>{
-    //router.get('/filterByStatus/:status',  async(req,res)=>{ //testing line - original above. Tested: Validates token - Good, empty table - Good,broken param - returns empty arr, proper param - Good
-
     const status = req.params.status;
     const filteredList = await user.findAll({where:{
         status: status
@@ -181,10 +193,10 @@ router.get('/filterByStatus/:status', validateToken, async(req,res)=>{
     res.json(filteredList);
 });
 
-// (GET)/Admin/Admins/:role - return info for a filtered list by status
+/**
+ *   (GET)/Admin/Admins/:role - return info for a filtered list by status
+ */
 router.get('/searchByUsername/:username', validateToken, async(req,res)=>{
-    //router.get('/searchByUsername/:username',  async(req,res)=>{ //testing line - original above. Tested: Validates token - Good, empty table - Good,broken param - returns empty arr, proper param - Good
-
     const username = req.params.username;
     const result = await user.findAll({where:{
         username: username
